2d0027b623785a1b24859d288cdf1e5819eff1ff
Marcantonio Moraes <marcantonio.lupercino@gmail.com>
2025-09-23 19:22:17 -0300

    Delete dist directory

--- dist/classes/Animal.d.ts
+++
@@ -1,9 +0,0 @@
-import type { IAnimal } from "../interfaces/iAnimal.js";
-export declare abstract class Animal implements IAnimal {
-    nome: string;
-    idade: number;
-    constructor(nome: string, idade: number);
-    abstract emitirSom(): void;
-    abstract mover(): void;
-}
-//# sourceMappingURL=Animal.d.ts.map
